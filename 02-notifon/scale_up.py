357e8024cd1dd08c2dad0bc886abbb8f933e41f3
Mauricio Roda <rodamauricio@gmail.com>
2019-10-28 16:00:47 -0400

    Autoscale automation

---
+++ 02-notifon/scale_up.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+import boto3
+session = boto3.Session(profile_name='shotty')
+as_client = session.client('autoscaling')
+as_client.describe_auto_scaling_groups()
+as_client.describe_policies()
+as_client.execute_policy(AutoScalingGroupName='Notifon Example Group', PolicyName='Scale Up')
